82fa353a338c13eb5ab5be74f0ff4b831f742fa9
thanhlam0822 <benzamano123@gmail.com>
2022-11-16 10:23:48 +0700

    front-end

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(ElementPlus).use(router).mount('#app')
---
+++ src/views/HeaderView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+   <div class="text-center">
+     <el-input
+         v-model="input1"
+         class="w-50 m-2"
+         size="large"
+         placeholder="Please Input"
+     />
+       <i class="bi bi-lightbulb"></i>
+
+
+       <i class="bi bi-bell-fill"></i>
+     <a href="">Login</a>
+     <a href="">Sign-in</a>
+   </div>
+
+  </div>
+</template>
+<script>
+  import {ref} from 'vue'
+  export default {
+    setup() {
+      let input1 = ref('')
+      return {
+        input1
+      }
+    }
+  }
+</script>
